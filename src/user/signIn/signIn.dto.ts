e3c1bad91f497ca1bca9c8938afd2dacd5b74311
overflowlink <overflowlink@gmail.com>
2023-07-09 11:49:38 +0900

    Feat: Make requests check parameters in Dto

--- src/user/signIn/signIn.dto.ts
+++ src/user/signIn/signIn.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class SignInDto {
+    @IsEmail()
+    @IsNotEmpty()
     userEmail: string;
+
+    @IsNotEmpty()
     userPassword: string;
 }
--- src/user/signUp/signUp.dto.ts
+++ src/user/signUp/signUp.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class SignUpDto {
+    @IsEmail()
+    @IsNotEmpty()
     userEmail: string;
+    
+    @IsNotEmpty()
     userPassword: string;
+
+    @IsNotEmpty()
     userName: string;
 }
